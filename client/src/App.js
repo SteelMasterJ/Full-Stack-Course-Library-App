eb8122818835e804145fd81ec4027adc84ff66f7
Joseph Steel <joseph.steel.iv@gmail.com>
2020-09-15 16:59:34 -0400

    UserSignIn Component Created

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
 import UnhandledError from './components/UnhandledError';
+import UserSignIn from './components/UserSignIn';
 
 // Higher order component
 import withContext from './Context';
@@ -16,6 +17,7 @@ import withContext from './Context';
 //subscribing components to context
 const CoursesWithContext = withContext(Courses);
 const CourseDetailWithContext = withContext(CourseDetail);
+const UserSignInWithContext = withContext(UserSignIn);
 
 
 function App() {
@@ -26,6 +28,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={CoursesWithContext} />
         <Route exact path="/courses/:id" component={CourseDetailWithContext}/>
+        <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/error" component={UnhandledError} />
       </Switch>
     </div>
